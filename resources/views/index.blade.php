7cd652fa08b9875300b28dd4e4d6437ef383a566
MD.AHOSAN HABIB PRANTO <77453098+ahosan105@users.noreply.github.com>
2021-04-30 12:01:04 +0600

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,447 @@
+@extends('master')
+
+@section('title')
+About-Law &mdash; Free Website Template, Free HTML5 Template by freehtml5.co
+@endsection
+
+@section('contant')
+<!-- slider  -->
+<aside id="fh5co-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url({{asset('frontend')}}/assets/images/img_bg_1.png);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Expert Legal Solutions</h1>
+									<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+									<p><a class="btn btn-primary btn-lg" href="#">Make An Appointment</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url({{asset('frontend')}}/assets/images/img_bg_2.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Business Law</h1>
+									<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+									<p><a class="btn btn-primary btn-lg btn-learn" href="#">Make An Appointment</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url({{asset('frontend')}}/assets/images/img_bg_3.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Defend Your Constitutional Right with Legal Help</h1>
+									<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+									<p><a class="btn btn-primary btn-lg btn-learn" href="#">Make An Appointment</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>		   	
+		  	</ul>
+	  	</div>
+	</aside>
+
+
+<!-- catagory  -->
+<div id="fh5co-practice" class="fh5co-bg-section">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>catagory</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+						<i class="fas fa-gavel"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Real Estate Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+						<i class="fas fa-car-crash"></i>
+							
+						</span>
+						<div class="desc">
+							<h3><a href="#">Insurance Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-shopping-cart"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Business Law</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+						<i class="fas fa-balance-scale"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Personal Injury</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-heart"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Medical Neglegence</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-help"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Criminal Defense</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-help"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Criminal Defense</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-help"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Criminal Defense</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p>
+							<a href="#" class="btn btn-primary">book now</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12 text-center animate-box">
+					<p><a class="btn btn-primary btn-lg btn-learn" href="#">View More</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- case  -->
+<div id="fh5co-project">
+
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Case Study</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus .</p> 
+		
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Business</h3>
+						<span>CEO. Hon Doe</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Legal Advice</h3>
+						<span>Atty. John Doe</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Read Bible</h3>
+						<span>Ptr. Jhon Doe</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Affidavit</h3>
+						<span>Atty. Boo Doe</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Affidavit</h3>
+						<span>Atty. Boo Doe</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Affidavit</h3>
+						<span>Atty. Boo Doe</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Sports</h3>
+						<span>Atty. Smith D</span>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="{{asset('frontend')}}/assets/images/project-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive img-fluid">
+						<h3>Photography</h3>
+						<span>Phtr. Arnt Tee</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- shabbir code  -->
+<!-- blog  -->
+
+<div id="fh5co-consult">
+        <div class="video fh5co-video" style="background-image: url({{asset('frontend')}}/assets/images/picture1.jpg);">
+        </div>
+        <div class="choose animate-box">
+            <div class="fh5co-heading">
+                <h2>Free Legal Consultation</h2>
+            </div>
+            <form action="#">
+                <div class="row form-group">
+                    <div class="col-md-6">
+                        <!-- <label for="fname">First Name</label> -->
+                        <input type="text" id="fname" class="form-control" placeholder="Your firstname">
+                    </div>
+                    <div class="col-md-6">
+                        <!-- <label for="lname">Last Name</label> -->
+                        <input type="text" id="lname" class="form-control" placeholder="Your lastname">
+                    </div>
+                </div>
+
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <!-- <label for="email">Email</label> -->
+                        <input type="text" id="email" class="form-control" placeholder="Your email address">
+                    </div>
+                </div>
+
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <!-- <label for="subject">Subject</label> -->
+                        <input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
+                    </div>
+                </div>
+
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <!-- <label for="message">Message</label> -->
+                        <textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Send Message" class="btn btn-primary">
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+<!-- home  -->
+<div id="fh5co-blog" class="fh5co-bg-section">
+
+        <div class="container">
+            <div class="row animate-box">
+                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                    <h2>Recent Post</h2>
+                    <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-3">
+                    <div class="fh5co-blog animate-box">
+                        <a href="#"><img class="img-responsive" src="{{asset('frontend')}}/assets/images/picture2.jpg" alt=""></a>
+                        <div class="blog-text">
+                            <h3><a href="#">Legal Consultation</a></h3>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3">
+                    <div class="fh5co-blog animate-box">
+                        <a href="#"><img class="img-responsive" src="{{asset('frontend')}}/assets/images/picture3.jpg" alt=""></a>
+                        <div class="blog-text">
+                            <h3><a href="#">Criminal Case</a></h3>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3">
+                    <div class="fh5co-blog animate-box">
+                        <a href="#"><img class="img-responsive" src="{{asset('frontend')}}/assets/images/picture4.jpg" alt=""></a>
+                        <div class="blog-text">
+                            <h3><a href="#">Business Law</a></h3>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3">
+                    <div class="fh5co-blog animate-box">
+                        <a href="#"><img class="img-responsive" src="{{asset('frontend')}}/assets/images/picture5.jpg" alt=""></a>
+                        <div class="blog-text">
+                            <h3><a href="#">Business Law</a></h3>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- attorny  -->
+        <div id="fh5co-about"> 
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Our Attorneys</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-4 text-center animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="{{asset('frontend')}}/assets/images/user-2.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Jean Smith</h3>
+						<strong class="role">Counsel</strong>
+						<p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 text-center animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="{{asset('frontend')}}/assets/images/user-2.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Hush Raven</h3>
+						<strong class="role">Head of International Practice</strong>
+						<p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 text-center animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="{{asset('frontend')}}/assets/images/user-2.jpg" alt="">
+						<h3>Alex King</h3>
+						<strong class="role">Managing Partner, Attorney</strong>
+						<p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+   
+    
+	<!-- counter  -->
+
+	<div id="fh5co-counter" class="fh5co-counters fh5co-bg-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 text-center animate-box">
+					<span class="icon"><i class="icon-user"></i></span>
+					<span class="fh5co-counter js-counter" data-from="0" data-to="275" data-speed="5" data-refresh-interval="50"></span>
+					<span class="fh5co-counter-label">Satisfied Clients</span>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<span class="icon"><i class="icon-speech-bubble"></i></span>
+					<span class="fh5co-counter js-counter" data-from="0" data-to="235" data-speed="50" data-refresh-interval="50"></span>
+					<span class="fh5co-counter-label">Cases Won</span>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<span class="icon"><i class="icon-trophy"></i></span>
+					<span class="fh5co-counter js-counter" data-from="0" data-to="50" data-speed="50" data-refresh-interval="50"></span>
+					<span class="fh5co-counter-label">Awards Won</span>
+				</div>
+				<div class="col-md-3 text-center animate-box">
+					<span class="icon"><i class="icon-users"></i></span>
+					<span class="fh5co-counter js-counter" data-from="0" data-to="258" data-speed="50" data-refresh-interval="50"></span>
+					<span class="fh5co-counter-label">Lawyers</span>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+    <div id="fh5co-started" style="background-image:url({{asset('frontend')}}/assets/images/picture6.jpg);">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row animate-box">
+                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                    <h2>Legal Advice</h2>
+                    <p>We help people effectively fight their offenders back and successfully defend their own stand!</p>
+                </div>
+            </div>
+            <div class="row animate-box">
+                <div class="col-md-8 col-md-offset-2 text-center">
+                    <p><a href="#" class="btn btn-default btn-lg">Consultation</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
